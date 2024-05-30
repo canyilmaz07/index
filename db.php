fdd6335069fc0b2a2f78113d14a519959849f9f9
canyilmaz07 <cnylmz735@gmail.com>
2024-05-30 17:45:04 +0300

    Update db.php

--- db.php
+++ db.php
@@ -10,15 +10,3 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Can</title>
-</head>
-<body>
-    
-</body>
-</html>
